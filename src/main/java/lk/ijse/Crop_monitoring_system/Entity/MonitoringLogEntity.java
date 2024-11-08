6afa5dc9425733235fc7f702abacfa27977c0821
fahim <fahim1998108@gmail.com>
2024-11-09 04:02:05 +0530

    Commit Monitoe ane Staff Entity

---
+++ src/main/java/lk/ijse/Crop_monitoring_system/Entity/MonitoringLogEntity.java
@@ -0,0 +1,14 @@
+package lk.ijse.Crop_monitoring_system.Entity;
+
+import java.util.Date;
+import java.util.List;
+
+public class MonitoringLogEntity {
+    private String Logcode;
+    private Date LogDate;
+    private String LogDetails;
+    private String ObservedImage;
+    private List Field;
+    private List Crop;
+    private List Staff;
+}
---
+++ src/main/java/lk/ijse/Crop_monitoring_system/Entity/VehicleEntity.java
@@ -0,0 +1,11 @@
+package lk.ijse.Crop_monitoring_system.Entity;
+
+public class VehicleEntity {
+    private String VehicleCode;
+    private String LicensePlateNumber;
+    private String VehicleCategory;
+    private String FuelType;
+    private String Status;
+    private String AllocatedStaffMemberDetails;
+    private String Remarks;
+}
