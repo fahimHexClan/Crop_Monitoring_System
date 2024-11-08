6494cb3c2e245afaa30bb4a9b48bac5654a7e862
fahim <fahim1998108@gmail.com>
2024-11-09 03:54:54 +0530

    Commit StaffEntity

---
+++ src/main/java/lk/ijse/Crop_monitoring_system/Entity/StaffEntity.java
@@ -0,0 +1,29 @@
+package lk.ijse.Crop_monitoring_system.Entity;
+
+import java.util.Date;
+import java.util.List;
+
+public class StaffEntity {
+    private String StaffId;
+    private String FirstName;
+    private String LastName;
+    private String Designation;
+    private Enum Gender;
+    private Date JoinedDate;
+    private Date DOB;
+    private String AddressLine01;
+    private String AddressLine02;
+    private String AddressLine03;
+    private String AddressLine04;
+    private String AddressLine05;
+    private String ContactNo;
+    private String Email;
+    private String Role;
+    private List Field;
+    private List Vehicle;
+
+
+
+
+
+}
