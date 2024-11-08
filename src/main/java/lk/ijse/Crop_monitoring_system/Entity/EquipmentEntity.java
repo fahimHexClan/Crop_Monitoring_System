eec881be5e31b35635c2c46662a53bd3bd167dc7
fahim <fahim1998108@gmail.com>
2024-11-09 04:04:31 +0530

    Commit Monitoe ane Staff Entity

---
+++ src/main/java/lk/ijse/Crop_monitoring_system/Entity/EquipmentEntity.java
@@ -0,0 +1,11 @@
+package lk.ijse.Crop_monitoring_system.Entity;
+
+public class EquipmentEntity {
+    private String EquipmentId;
+    private String Name;
+    private String Type;
+    private String Status;
+    private String AssignedStaffDetails;
+    private String AssignedFieldDetails;
+
+}
